d88608bd6beb99038fd4e6363d7844bb8cb3ba2d
Sara Sh <219410319@psu.edu.sa>
2023-05-10 23:46:32 +0300

    updated

--- routes/ExpertRoutes.js
+++ routes/ExpertRoutes.js
@@ -10,8 +10,8 @@ const upload = require('../uploadImages')
 router.get("/FormExpert", ExpertController.FormExpert);
 router.post("/FormExpert", upload.single('Picture'), ExpertController.FormExpert_post);
 
-// router.get("/LogInExpert", ExpertController.loginExperts);
-// router.post("/LogInExpert", ExpertController.loginExperts_post);
+router.get("/LogInExpert", ExpertController.loginExperts);
+router.post("/LogInExpert", ExpertController.loginExperts_post);
 router.get("/LoginExpert", ExpertController.loginExperts);
 router.post("/LoginExpert", ExpertController.loginExperts_post);
 
